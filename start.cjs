7a8356aff97759ed3d830f78d3db59feca058a3b
yuebaiguazi-wq <yuebaiguazi@gmail.com>
2026-04-09 11:48:19 +0800

    Update and rename start.js to start.cjs

--- start.cjs
+++ start.cjs
@@ -1,12 +1,14 @@
 const { spawn } = require('child_process');
 
+console.log('启动管理器开始运行...');
+
 // 启动keepalive
-const keepalive = spawn('node', ['keepalive.js'], {
+const keepalive = spawn('node', ['keepalive.cjs'], {
   stdio: 'inherit'
 });
 
 // 启动ST服务器
-const server = spawn('node', ['server.js'], {
+const server = spawn('node', ['server.js', '--listen', '--disableCsrf'], {
   stdio: 'inherit'
 });
 
@@ -21,4 +23,5 @@ server.on('error', (err) => {
 process.on('SIGTERM', () => {
   keepalive.kill();
   server.kill();
+  process.exit(0);
 });
